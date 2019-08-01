c4dd696ea3a7a7f118aa267d2edc9cbc251b05e8
Jan-Pieter Baert <janpieter.baert@ugent.be>
2019-08-01 20:17:58 +0200

    add screensaver timeout script

---
+++ screensaver-timeout.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+"""
+A small program to have a few different screensaver timeout modes
+TODO: add a modes|timeouts file
+"""
+
+file=$HOME/.presenter-mode
+modes=("Default mode" "Code mode" "Presenter mode")
+timeouts=("60 0" "300 0" "0 0")
+length=${#modes[@]}
+
+if  [ ! -f $file ] || [ -z "$(cat ${file})" ]
+then
+	echo "file doesn't exists or is empty"
+	index=0
+else
+	source $file
+	index=$((($index+1)%$length))
+fi
+
+if [ "$1" == "MODE" ]
+then
+    echo $mode
+	exit
+elif [ -n "$1" ] && [ "$1" -eq "$1" ]
+then
+    if [ $1 -lt $length ]
+	then
+		index=$1
+		echo "updating with index $index"
+    else
+		echo "your new index should be less than $length"
+    fi
+fi
+
+
+
+xset s $timeouts[index]
+
+rm $file
+touch $file
+echo "mode='${modes[${index}]}'">>$file
+echo "index=${index}">>$file
+
+echo "Old mode: $mode"
+echo "New mode: ${modes[index]}"
