b71bab8e459a0871473aadd6a7f9700dd1689c3f
Jan-Pieter Baert <janpieter.baert@ugent.be>
2019-12-26 19:05:29 +0100

    Add mp3 file so you don't have to download it every time

--- blok-pauze.sh
+++ blok-pauze.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export TIMEOUT=$1
 sleep $TIMEOUT
-mpv https://www.youtube.com/watch\?v\=Wp0_Y72ARlA --loop-file
+mpv /home/jan-pieter/git_repos/custum_scripts/alarm.mp3 --loop-file
 exit
