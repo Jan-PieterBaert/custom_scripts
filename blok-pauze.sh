58b9db0b2204a60019a50675c19dc248b0bc03b1
Jan-Pieter Baert <janpieter.baert@ugent.be>
2020-01-04 11:37:35 +0100

    Small fixes

--- blok-pauze.sh
+++ blok-pauze.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 export TIMEOUT=$1
 sleep $TIMEOUT
-mpv /home/jan-pieter/git_repos/custum_scripts/alarm.mp3 --loop-file
-exit
+mpv /home/jan-pieter/git_repos/custum_scripts/alarm.mp3 --loop-file --no-resume-playback
+
+
+counter_location="/home/jan-pieter/blok_pauze_counter"
+
+# increase the counter
+touch $counter_location
+current="$(cat $counter_location)"
+
+# A non-number string + 1 will give 1
+new_value=$(($current + $TIMEOUT))
+echo $new_value > $counter_location
